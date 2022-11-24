1790835fc2a6b532d05a3a02d7a5d648664f8900
Aleksayshn <112755313+Aleksayshn@users.noreply.github.com>
2022-11-24 08:52:31 +0000

    Deploying to gh-pages from @ Aleksayshn/team-proj-IceCream@797a32e9c6fc6ba07128e25d866195de3aadba9c 🚀

---
+++ index.3f2d53c1.js
@@ -0,0 +1,2 @@
+(()=>{const e={openMenuBtn:document.querySelector("[data-menu-open]"),closeMenuBtn:document.querySelector("[data-menu-close]"),menu:document.querySelector("[data-menu]"),body:document.querySelector("body")};function n(){e.menu.classList.toggle("is-hidden"),e.body.classList.remove("no-scroll")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)})();
+//# sourceMappingURL=index.3f2d53c1.js.map
---
+++ index.841b6709.js
@@ -0,0 +1,2 @@
+!function(){var e={openMenuBtn:document.querySelector("[data-menu-open]"),closeMenuBtn:document.querySelector("[data-menu-close]"),menu:document.querySelector("[data-menu]"),body:document.querySelector("body")};function n(){e.menu.classList.toggle("is-hidden"),e.body.classList.remove("no-scroll")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)}();
+//# sourceMappingURL=index.841b6709.js.map
