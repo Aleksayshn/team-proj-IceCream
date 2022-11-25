915cecfadc0b70b18bbc5fbd3987795a23511dd3
Aleksayshn <112755313+Aleksayshn@users.noreply.github.com>
2022-11-25 13:19:46 +0000

    Deploying to gh-pages from @ Aleksayshn/team-proj-IceCream@326013f874223a474b8f60dc4b2ac46d65aadd3a 🚀

--- index.841b6709.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openMenuBtn:document.querySelector("[data-menu-open]"),closeMenuBtn:document.querySelector("[data-menu-close]"),menu:document.querySelector("[data-menu]"),body:document.querySelector("body")};function n(){e.menu.classList.toggle("is-hidden"),e.body.classList.remove("no-scroll")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)}();
-//# sourceMappingURL=index.841b6709.js.map
