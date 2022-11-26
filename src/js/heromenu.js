0e18aa6a7af6ff5deba1868e82f1fb19c224c5fb
Daria Hryshchenko <daria199210@gmail.com>
2022-11-27 00:12:50 +0200

    add easy animation for cursor

--- src/js/heromenu.js
+++ src/js/heromenu.js
@@ -33,3 +33,20 @@ valueDisplays.forEach((valueDisplay) => {
 
   }, duration);
 });
+
+
+////CURSOR
+
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+  cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
+})
+
+document.addEventListener('click', () => {
+  cursor.classList.add("expand");
+
+  setTimeout(() => {
+    cursor.classList.remove("expand");
+  }, 500)
+})
